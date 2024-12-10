0ea43f48aa798a5a3bc6331a0ce1cb958f6657a2
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-10 21:45:03 +0100

    Rooms names

--- src/utils/eventMapper.js
+++ src/utils/eventMapper.js
@@ -1,7 +1,7 @@
 // Function to map event data for calendar
 export const mapEventData = (data) => ({
     id: data.id,
-    title: `Room ${data.room_id}`,
+    title: `Room ${data.room_name}`,
     start: new Date(data.start_time),
     end: new Date(data.end_time),
   });
