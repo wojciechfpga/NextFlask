19a2347d92205bf6032dadd107269a8c00eae1f2
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-10 17:26:10 +0100

    Calendar work

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -1,24 +1,22 @@
-'use client'
+'use client';
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { sendReservation } from "src/services/apiService";
 import { useSelector } from "react-redux";
-import { useState } from "react";
-// Helper function to format Date to "YYYY-MM-DDTHH:mm" for datetime-local input
+
+
 const formatDateForInput = (date) => {
   if (!date) return "";
   return new Date(date).toISOString().slice(0, 16); // Adjust to remove seconds
 };
 
 const ReservationForm = ({ slot = [] }) => {
-  const { user, token } = useSelector((state) => state.auth);
-  const [valueStart,setValueStart]=useState(new Date())
-  const [valueEnd,setValueEnd]=useState(new Date())
-  const { register, handleSubmit, reset } = useForm({
+  const { token } = useSelector((state) => state.auth);
+  const { register, handleSubmit, reset, setValue } = useForm({
     defaultValues: {
       room_id: "",
-      start_time: formatDateForInput(slot[0]), 
-      end_time: formatDateForInput(slot[1]), 
+      start_time: formatDateForInput(slot[0]),
+      end_time: formatDateForInput(slot[1]),
     },
   });
 
@@ -27,24 +25,17 @@ const ReservationForm = ({ slot = [] }) => {
     reset();
   };
 
-  const handleNewValueStart = (event) => {
-     setValueStart(event.target.value);
-    };
+  useEffect(() => {
+    console.log("Slot in")
+    console.log(slot)
+    if (slot.start && slot.end) {
+      console.log("Slot 0")
+      console.log(formatDateForInput(slot.start))
+      setValue("start_time", formatDateForInput(slot.start));
+      setValue("end_time", formatDateForInput(slot.end));
+    }
+  }, [slot]);
 
-    const handleNewValueEnd = (event) => {
-      setValueEnd(event.target.value);
-     };
-
-  useEffect(()=>{
-    console.log("Useeffect")
-    console.log(slot.start.toISOString().substring(0, 16))
-    console.log("End effect")
-    setValueStart(slot.start.toISOString().substring(0, 16))
-    setValueEnd(slot.end.toISOString().substring(0, 16))
-  },[slot])
-  console.log("SLOT:", slot);
-
-  
   return (
     <div className="bg-white rounded-lg shadow-lg p-6 max-w-md w-full">
       <h2 className="text-xl font-bold text-gray-700 mb-4">Book Room</h2>
@@ -65,8 +56,6 @@ const ReservationForm = ({ slot = [] }) => {
           </label>
           <input
             type="datetime-local"
-            value={valueStart}
-            onChange={handleNewValueStart}
             className="w-full px-4 py-2 border rounded-lg"
             {...register("start_time", { required: true })}
           />
@@ -77,8 +66,6 @@ const ReservationForm = ({ slot = [] }) => {
           </label>
           <input
             type="datetime-local"
-            value={valueEnd}
-            onChange={handleNewValueEnd}
             className="w-full px-4 py-2 border rounded-lg"
             {...register("end_time", { required: true })}
           />
