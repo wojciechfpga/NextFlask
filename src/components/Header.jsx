028328b230eac2bd503c0f8691bac3330b34e0ed
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-11 21:33:19 +0100

    Room list Refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,13 @@
-import Link from "next/link";
+import HeaderMainLink from "./HeaderSubComponets/HeaderMainLink"
+import HeaderRoomsLink from "./HeaderSubComponets/HeaderRoomsLink"
 
 const HeaderLayout = ({ children }) => {
     return (
         <header className="bg-gray-900 text-white py-4 px-8 shadow-md border-b border-gray-700">
             <div className="flex justify-between items-center">
                 <div className="flex items-center">
-                    <Link href="/">
-                        <h1 className="text-xl font-bold tracking-wide mr-8">Rooms Reservation</h1>
-                    </Link>
-                    <Link href="/rooms">
-                        <h2 className=" text-white py-2 px-4 border-none">
-                            Our rooms
-                        </h2>
-                    </Link>
+                <HeaderMainLink/>
+                <HeaderRoomsLink/>
                 </div>
                 <div>{children}</div>
             </div>
